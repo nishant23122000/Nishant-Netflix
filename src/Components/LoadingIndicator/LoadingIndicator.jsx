864630b46f37c12e4dc3dfc801f21bbc9978ccf4
nishant23122000 <nkpatel23122000@gmail.com>
2024-05-24 15:12:43 +0530

    chore: added LoadingIndicator Component

---
+++ src/Components/LoadingIndicator/LoadingIndicator.jsx
@@ -0,0 +1,18 @@
+import "./loading-indicator.css";
+
+function LoadingIndicator() {
+  return (
+    <div class="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default LoadingIndicator;
